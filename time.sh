e155736af77985a307c5d2d893b8ae3118f3ad63
veratrum <veratrum@users.noreply.github.com>
2019-08-25 13:15:37 +1200

    Modify script

--- time.sh
+++ time.sh
@@ -9,7 +9,7 @@
 # Nodes_9, 5: 0.06 seconds
 # Nodes_10, 20: 0.13 seconds
 # Nodes_11, 2: 6.23 seconds
-# Parallel BNB:
+# Parallel BNB (4 processors):
 # Nodes_8, 10: 0.01 seconds
 # Nodes_9, 5: 0.06 seconds
 # Nodes_10, 20: 0.15 seconds
@@ -21,7 +21,7 @@ echo "Nodes_9, 5: $(java -jar bin/process-scheduler.jar test_data/Nodes_9_Series
 echo "Nodes_10, 20: $(java -jar bin/process-scheduler.jar test_data/Nodes_10_Random.dot 20 -p 1 | grep -Po '[\d]+\.\d\d seconds')"
 echo "Nodes_11, 2: $(java -jar bin/process-scheduler.jar test_data/Nodes_11_OutTree.dot 2 -p 1 | grep -Po '[\d]+\.\d\d seconds')"
 
-echo "Parallel BNB:"
+echo "Parallel BNB (4 processors):"
 echo "Nodes_8, 10: $(java -jar bin/process-scheduler.jar test_data/Nodes_8_Random.dot 10 -p 4 | grep -Po '[\d]+\.\d\d seconds')"
 echo "Nodes_9, 5: $(java -jar bin/process-scheduler.jar test_data/Nodes_9_SeriesParallel.dot 5 -p 4 | grep -Po '[\d]+\.\d\d seconds')"
 echo "Nodes_10, 20: $(java -jar bin/process-scheduler.jar test_data/Nodes_10_Random.dot 20 -p 4 | grep -Po '[\d]+\.\d\d seconds')"
